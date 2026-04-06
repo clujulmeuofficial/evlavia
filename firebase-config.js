278ca0a6c971c5dbbd920cad9d5d2705745cc8d5
clujulmeuofficial <clujulmeuofficial@gmail.com>
2026-04-06 11:36:26 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,18 +3,17 @@
 //  Înlocuiește valorile de mai jos cu cele din consola Firebase
 //  după ce creezi proiectul (vezi GHID-FIREBASE.md)
 // ============================================================
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey:            "PUNE_API_KEY_AICI",
-  authDomain:        "PUNE_AUTH_DOMAIN_AICI",
-  projectId:         "PUNE_PROJECT_ID_AICI",
-  storageBucket:     "PUNE_STORAGE_BUCKET_AICI",
-  messagingSenderId: "PUNE_MESSAGING_SENDER_ID_AICI",
-  appId:             "PUNE_APP_ID_AICI"
+  apiKey: "AIzaSyDtLNfazILwGHpyHe_4yvKK7mUAdeJAkh4",
+  authDomain: "evlavia-cluj.firebaseapp.com",
+  projectId: "evlavia-cluj",
+  storageBucket: "evlavia-cluj.firebasestorage.app",
+  messagingSenderId: "918756184791",
+  appId: "1:918756184791:web:1679752fc700d6011dd692"
 };
 
 const app  = initializeApp(firebaseConfig);
